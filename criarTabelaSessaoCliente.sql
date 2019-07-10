46b1a344b1690a1a1c4fe07e0dc335cce4ad8c5f
Rieg <47364961+LeoRieg@users.noreply.github.com>
2019-07-10 19:25:43 -0300

    Update criarTabelaSessaoCliente.sql

--- criarTabelaSessaoCliente.sql
+++ criarTabelaSessaoCliente.sql
@@ -5,9 +5,4 @@ CREATE TABLE IF NOT EXISTS Cliente_Sessao (
  PRIMARY KEY (Cliente_idCliente, id_Sessao, id_Filme),
  INDEX fk_Cliente_has_Sessao_Sessao1_idx (id_Sessao ASC, id_Filme ASC),
  INDEX fk_Cliente_has_Sessao_Cliente1_idx (Cliente_idCliente ASC),
- CONSTRAINT fk_Cliente_has_Sessao_Cliente1
-   FOREIGN KEY (Cliente_idCliente)
-   REFERENCES Cliente (idCliente),
- CONSTRAINT fk_Cliente_has_Sessao_Sessao1
-   FOREIGN KEY (id_Sessao , id_Filme)
-   REFERENCES Sessao (idSessao , Filme_idFilme));
+ CONSTRAINT fk_Cliente_has_Sessao_Cliente1);
