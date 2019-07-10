1d70b244a94d36ff0ecfe2fd88ebedbd65ceb8dc
Rieg <47364961+LeoRieg@users.noreply.github.com>
2019-07-10 19:26:03 -0300

    Update criarTabelaSessao.sql

--- criarTabelaSessao.sql
+++ criarTabelaSessao.sql
@@ -5,8 +5,4 @@ CREATE TABLE IF NOT EXISTS Sessao (
  Preco_Sessao FLOAT(3) NULL,
  Assento_Sessao INT(3) NULL,
  Filme_idFilme INT NOT NULL,
- PRIMARY KEY (idSessao, Filme_idFilme),
- INDEX fk_Sessao_Filme_idx (Filme_idFilme ASC),
- CONSTRAINT fk_Sessão_Filme
-   FOREIGN KEY (Filme_idFilme)
-   REFERENCES Filme (idFilme));
+ PRIMARY KEY (idSessao, Filme_idFilme));
